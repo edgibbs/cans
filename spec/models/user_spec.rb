0d902a00bc8504d875f70a2bb2f8f770bdf1f804
Ed Gibbs <edward_gibbs@yahoo.com>
2018-05-02 06:04:26 -0700

    Add rubocop with clean run

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe User do
-  it "exists with first and last name" do
+  it 'exists with first and last name' do
     user = User.new(first_name: 'George', last_name: 'Burdell')
     user.save
     expect(user.reload.first_name).to eq 'George'
